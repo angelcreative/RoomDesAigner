8253efcd96082b83c984b1bde6e6172172af0ee2
Angel J. Sanchez <angelsanchez@socialbro.com>
2024-03-29 22:35:06 +0100

    updated code hope works

--- app/static/script.js
+++ app/static/script.js
@@ -300,7 +300,7 @@ function generateImages(imageUrl, selectedValues, isImg2Img) {
     .map(([key, value]) => `${key}: ${value}`)
     .join(", ");
 
-  const promptEndy = `.  ${selectedPicture},  Transform the provided empty room image by adding furniture and decor, ensuring utmost adherence to the room's original perspective, structural integrity, and alignment with existing architectural features like windows, doors, and light sources. The goal is to create a harmonious and fully furnished living space that seamlessly blends new additions with the room's inherent structure. Each piece introduced must complement the architecture and natural lighting, filling the space without leaving empty areas, and maintaining a realistic appearance as if these elements were always part of the original setup. Achieve a cohesive design that respects the room's layout and angles, enhancing its functionality and aesthetic appeal, making the space inviting and visually compelling. `;
+  const promptEndy = `. ${selectedPicture},  `;
   const aspectRatio = document.querySelector('input[name="aspectRatio"]:checked').value;
   const width = aspectRatio === "portrait" ? 512 : 1024;
   const height = aspectRatio === "portrait" ? 1024 : 512;
