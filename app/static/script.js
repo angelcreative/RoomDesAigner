5de1e7dafa68f7ba1b9c896bc39ea6f49493a68b
Angel J. Sanchez <angelsanchez@socialbro.com>
2024-04-03 19:32:46 +0200

    normal brutality

--- app/static/script.js
+++ app/static/script.js
@@ -324,7 +324,7 @@ function generateImages(imageUrl, selectedValues, isImg2Img) {
     //width: width,
     //height: height,
     width: "1024",
-    height: "800",
+    height: "512",
     samples: "1",
     
     num_inference_steps: "50",
