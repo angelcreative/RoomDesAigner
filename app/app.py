76e032bfc6ca031c1cdfd4e9a1c1702736fac5c3
Angel J. Sanchez <angelsanchez@socialbro.com>
2024-05-31 11:30:19 +0200

    Psnton

--- app/app.py
+++ app/app.py
@@ -41,7 +41,7 @@ def transform_prompt(prompt_text):
     messages = [
        {
             "role": "system",
-            "content": "You are a helpful assistant that transforms lists of values into natural language descriptions. When encountering hex color values, convert them into their corresponding color names."
+            "content": "You are a helpful assistant that transforms lists of values into natural language descriptions. When encountering hex color values, convert them into their corresponding color Pantone."
         },
         {
             "role": "user",
