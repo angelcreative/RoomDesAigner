2ae1ce43caba0baa9addf98f69b8b72676bfae93
angelcreative <ajsc50@gmail.com>
2024-06-17 12:16:02 +0200

    fake image 3

--- app/static/script.js
+++ app/static/script.js
@@ -337,6 +337,7 @@ console.log(`Width: ${width}, Height: ${height}`);
     guidance_scale: "10",
     num_inference_steps: "40", 
     seed: seedValue,
+    "model_id": "juggernaut-xl",
     webhook: null,
     safety_checker: false, 
     track_id: null,
