d7c797a267cd23dac846e884f16e28400667afdf
angelcreative <ajsc50@gmail.com>
2024-06-23 22:29:19 +0200

    clothes

--- app/static/script.js
+++ app/static/script.js
@@ -301,7 +301,7 @@ function generateImages(imageUrl, selectedValues, isImg2Img) {
     .map(([key, value]) => `${key}: ${value}`)
     .join(", ");
 
-  const promptEndy = `dense furnishings and decorations.`;
+  const promptEndy = `walls adorned with trendy wallpaper or framed pictures, creating a stylish and modern ambiance. dense furnishings and decorations.`;
   
  
 
