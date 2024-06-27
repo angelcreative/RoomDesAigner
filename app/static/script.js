eefcf2f89b1f2e88c30881a75ec257db6fbd870d
angelcreative <ajsc50@gmail.com>
2024-06-27 12:00:28 +0200

    epicrealism-v4

--- app/static/script.js
+++ app/static/script.js
@@ -294,7 +294,7 @@ function generateImages(imageUrl, selectedValues, isImg2Img) {
   const customText = document.getElementById("customText").value;
   const pictureSelect = document.getElementById("imageDisplayUrl");
   const selectedPicture = pictureSelect.value;
-    const promptInit = `Kodak Ektra 100 +1 stop, RAW, unedited, symmetrical balance, in-frame, 8k, hyperrealistic, highly detailed, cinematic lighting, stunningly beautiful, intricate, (professionally color graded), ((bright soft diffused light)), volumetric fog, HDR 4K, 8K.` ;
+    const promptInit = `Kodak Ektra 100 +1 stop, sharp focus, RAW, unedited, symmetrical balance, in-frame, 8k, hyperrealistic, highly detailed, cinematic lighting, stunningly beautiful, intricate, (professionally color graded), ((bright soft diffused light)), volumetric fog, HDR 4K, 8K.` ;
     //detailed skin texture, detailed clothing, 8K hyperrealistic, full body, detailed clothing, highly detailed, cinematic lighting, stunningly beautiful, intricate, sharp focus, f/1. 8, 85mm, (centered image composition), (professionally color graded), ((bright soft diffused light)), volumetric fog, trending on instagram, trending on tumblr, HDR 4K, 8K
 //beautiful bright eyes, highly detailed eyes, realistic skin, detailed clothing, ultra detailed skin texture,
 //    "prompt": "ultra realistic close up portrait ((beautiful pale cyberpunk female with heavy black eyeliner)), blue eyes, shaved side haircut, hyper detail, cinematic lighting, magic neon, dark red city, Canon EOS R3, nikon, f/1.4, ISO 200, 1/160s, 8K, RAW, unedited, symmetrical balance, in-frame, 8K",
@@ -363,7 +363,7 @@ const prompt = {
   use_karras_sigmas: "yes",
   tomesd: "yes",
   seed: seedValue,
-  model_id:"epicrealismnew",
+  model_id:"epicrealism-v4",
   lora_model:"xl_more_enhancer",
   lora_strength:"1",
   //model_id: modelId,  
