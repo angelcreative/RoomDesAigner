c6f6286959cfb15998124776d8297681a56a1350
angelcreative <ajsc50@gmail.com>
2024-08-05 11:23:03 +0200

    lora

--- app/static/script.js
+++ app/static/script.js
@@ -438,7 +438,7 @@ const prompt = {
   tomesd: "yes",
   seed: seedValue,
   model_id: modelId,
-  lora_model: lora,
+  lora: lora,
   lora_strength: lora_strength,
   scheduler: "DPMSolverMultistepScheduler",
   webhook: null,
