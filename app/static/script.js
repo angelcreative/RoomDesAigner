51ad9c5d0d0763582e22b3a00dfa21db85aeee08
Angel J. Sanchez <angelsanchez@socialbro.com>
2024-09-02 12:54:40 +0200

    flux back

--- app/static/script.js
+++ app/static/script.js
@@ -545,7 +545,7 @@ console.log(`Width: ${width}, Height: ${height}`);
 
 // Determine the model_id based on the selection of the "person" field
 
-/*
+ 
 // Get selected models from the form
 const personValue = document.getElementById("personModel").value;
 const furnitureValue = document.getElementById("furnitureModel").value;
@@ -570,7 +570,7 @@ if (modelId === personValue) {
 } else if (modelId === furnitureValue) {
   lora = "u5-interior-design,clothingadjustloraap,xl_more_enhancer,detail-tweaker-xl";
 }  
-*/
+ 
   
 // Now build the JSON object with the updated values
 const prompt = {
@@ -585,9 +585,9 @@ const prompt = {
   use_karras_sigmas: "yes",
   tomesd: "yes",
   seed: seedValue,
-  model_id: "fluxydev",
-  //lora_model: lora,
- // lora_strength: lora_strength,
+  model_id: modelId,
+   lora_model: lora,
+ lora_strength: lora_strength,
   scheduler: "DPMSolverMultistepScheduler",
   webhook: null,
   safety_checker: "no",
