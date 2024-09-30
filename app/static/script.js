c47bca844851c75042462df8c3783facdc921669
angelcreative <ajsc50@gmail.com>
2024-09-30 13:00:44 +0200

    fluxschnelldev

--- app/static/script.js
+++ app/static/script.js
@@ -611,8 +611,8 @@ const evolutionCycle = document.getElementById("evolutionCycleCheckbox").checked
     } else if (selectedModel === "fluxdev") {
         modelConfig = {
             model_id: "fluxdev",
-            lora_model: null,
-            lora_strength: null
+            lora_model: "fluxschnelldev",
+            lora_strength: 0.7
         };
     }
     
