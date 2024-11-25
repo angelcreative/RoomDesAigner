397e39ffd7b1088b84cade5e3ac164d2de6981a7
angelcreative <ajsc50@gmail.com>
2024-11-25 10:05:19 +0100

    ultrarealistic-lora-project

--- app/static/script.js
+++ app/static/script.js
@@ -614,16 +614,16 @@ function getModelConfig(selectedModel) {
         "flux": { model_id: "flux", lora_model: null, lora_strength: null },
         "fluxdev": { model_id: "fluxdev", lora_model: null, lora_strength: null },
         "simplevectorflux": { model_id: "fluxdev", lora_model: "simplevectorflux", lora_strength: "1" },
-        "flux-detaile": { model_id: "fluxdev", lora_model: "flux-detaile,polyhedron-flux", lora_strength: "1,0.7" },
-        "fluxpro-11": { model_id: "fluxdev", lora_model: "fluxpro-11,polyhedron-flux", lora_strength: "1,0.7" },
+        "flux-detaile": { model_id: "fluxdev", lora_model: "flux-detaile,polyhedron-flux,ultrarealistic-lora-project", lora_strength: "1,0.7,0.5" },
+        "fluxpro-11": { model_id: "fluxdev", lora_model: "fluxpro-11,polyhedron-flux,ultrarealistic-lora-project", lora_strength: "1,0.7,0.5" },
         "fluxdevfashion": { model_id: "fluxdev", lora_model: "flux-fashion,polyhedron-flux", lora_strength: "1,0.7" },
-        "mystic": { model_id: "mystic", lora_model: "flux-detaile,polyhedron-flux", lora_strength: "0.9,1" },
+        "mystic": { model_id: "mystic", lora_model: "flux-detaile,polyhedron-flux,ultrarealistic-lora-project", lora_strength: "0.9,1,0.5" },
         "iphone-photo-flux-realism-booster": {
             model_id: "fluxdev",
-            lora_model: "iphone-photo-flux-realism-booster,polyhedron-flux",
-            lora_strength: "1,0.7"
+            lora_model: "iphone-photo-flux-realism-booster,polyhedron-flux,ultrarealistic-lora-project",
+            lora_strength: "1,0.7,0.5"
         },
-        "uncensored-flux-lora": { model_id: "fluxdev", lora_model: "uncensored-flux-lora,polyhedron-flux,flux-detaile", lora_strength: "0.5,1,0.9" },
+        "uncensored-flux-lora": { model_id: "fluxdev", lora_model: "uncensored-flux-lora,polyhedron-flux,flux-detaile,ultrarealistic-lora-project", lora_strength: "0.5,1,0.9,0.5" },
     };
 
     if (!models[selectedModel]) {
