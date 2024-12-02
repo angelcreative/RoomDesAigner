ff21f3c90d8d7059eb2926439fa836a4c13dac87
angelcreative <ajsc50@gmail.com>
2024-12-02 14:04:06 +0100

    woman to .4

--- app/app.py
+++ app/app.py
@@ -1019,7 +1019,7 @@ def generate_baby_face():
             "init_image": husband_url,
             "ip_adapter_id": "ip-adapter_sd15",
             "ip_adapter_image": wife_url,
-            "ip_adapter_scale": 0.6,
+            "ip_adapter_scale": 0.4,
             "prompt": prompt,
             "width": 512,
             "height": 512,
